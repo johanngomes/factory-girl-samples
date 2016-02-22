ead415936e5d5f804c41fc94fe45a97db5dd166b
johanngomes <johanngomes@gmail.com>
2016-02-22 13:40:11 -0300

    Add tests to verify the article building, published or unpublished

--- article-app/spec/models/article_spec.rb
+++ article-app/spec/models/article_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   it 'should build an published article' do
-    build :article, :published
+    article = build :article, :published
+    expect(article.published).to eq true
   end
 
   it 'should build an unpublished article' do
-    build :article, :unpublished
+    article = build :article, :unpublished
+    expect(article.published).to eq false
   end
 end
